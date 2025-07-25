dde5fd2a65150983cd23bbefa0d07dc717be2d43
Thao Tran <>
2025-07-25 22:35:41 +0200

    fix: fix server error

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const jsonServer = require("json-server");
-const express = require("express");
 const path = require("path");
 
 // config
-const server = express();
+const server = jsonServer.create();
+
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
 
 const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults();
+server.use(jsonServer.bodyParser);
+server.use(router);
 
-server.use("/", middlewares, router);
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
